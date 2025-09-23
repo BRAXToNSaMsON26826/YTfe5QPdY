c09761a07ee67c28204843f768f844f8935f92fa
BRAXToNSaMsON26826 <unready-motto0x@icloud.com>
2025-09-23 09:50:35 +0800

    Introduce Excel表格自动生成器 using UNITY

---
+++ ExcelGenerator_0923_0950_olk.cs
@@ -0,0 +1,18 @@
+// 代码生成时间: 2025-09-23 09:50:34
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Data;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelGenerator
+{
+    /// <summary>
+    /// Excel表格自动生成器类
+    /// </summary>
+    public class ExcelGenerator
+    {
+        /// <summary>
+        /// 生成Excel文件
+        /// </summary>
+        /// <param name=\
